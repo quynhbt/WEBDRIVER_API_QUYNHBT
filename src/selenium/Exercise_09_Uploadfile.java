3af93b2712f7084a535a26e650cba3a49db22a98
quynhbt <buihanh887@gmail.com>
2019-04-11 16:47:40 +0700

    done TC3_4 Ex9

--- src/selenium/Exercise_09_Uploadfile.java
+++ src/selenium/Exercise_09_Uploadfile.java
@@ -1,10 +1,14 @@
 package selenium;
 
-
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.List;
-import java.util.Set;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import org.testng.Assert;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -12,7 +16,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -30,20 +37,38 @@ public class Exercise_09_Uploadfile {
     String fileNamePath02 = rootFolder + "\\Upload file\\" + fileName02;
     String fileNamePath03 = rootFolder + "\\Upload file\\" + fileName03;
     
+    String chromePath = rootFolder + "\\Upload file\\chrome.exe";
+    String firefoxPath = rootFolder + "\\Upload file\\firefox.exe";
+    String iePath = rootFolder + "\\Upload file\\ie.exe";
+    
+    		
     String[] files = {fileNamePath01, fileNamePath02, fileNamePath03};
 
 
     @BeforeTest
-    public void beforeTest() {
-//        driver = new FirefoxDriver();
-    	System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
-    	driver = new ChromeDriver();
-    	driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-    	driver.manage().window().maximize();
+	public void beforeTest() {
+
+		// driver = new FirefoxDriver();
+
+		// Run on fifox lastest
+		 System.setProperty("webdriver.gecko.driver", ".\\lib\\geckodriver.exe");
+		 driver = new FirefoxDriver();
+
+		// Run with chorme
+//		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+//		driver = new ChromeDriver();
+
+		// Run on iE
+//		 System.setProperty("webdriver.ie.driver", ".\\lib\\IEDriverServer.exe");
+//		 driver = new InternetExplorerDriver();
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
     	
-    	System.out.println("File path 01 = " + fileNamePath01);
-    	System.out.println("File path 02 = " + fileNamePath02);
-    	System.out.println("File path 03 = " + fileNamePath03);
+//    	System.out.println("File path 01 = " + fileNamePath01);
+//    	System.out.println("File path 02 = " + fileNamePath02);
+//    	System.out.println("File path 03 = " + fileNamePath03);
 
     }
     
@@ -85,9 +110,10 @@ public class Exercise_09_Uploadfile {
     	
     }
     
-    @Test
+    
     public void TC_02_SendKeys_Multiple_A_Time() throws Exception  {
     	driver.get("http://blueimp.github.com/jQuery-File-Upload/");
+    	Thread.sleep(5000);
 
     	WebElement uploadFile = driver.findElement(By.xpath("//input[@type='file']"));
     	uploadFile.sendKeys(fileNamePath01 +  "\n" + fileNamePath02 + "\n" + fileNamePath03);
@@ -102,14 +128,80 @@ public class Exercise_09_Uploadfile {
     	Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + fileName01 + "']")).isDisplayed());
     	Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + fileName02 + "']")).isDisplayed());
     	Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + fileName03 + "']")).isDisplayed());
-    	
-    	
+  
     }
 
+    
+    public void TC_03_UploadFilebyAutoIT() throws Exception {
+    	driver.get("http://blueimp.github.com/jQuery-File-Upload/");
+    	
+    	if(driver.toString().contains("chrome")) {
+        	WebElement uploadFile = driver.findElement(By.cssSelector(".fileinput-button"));
+        	uploadFile.click();
+        	Thread.sleep(1000);
+        	//execute file exe
+        	Runtime.getRuntime().exec(new String[] { chromePath, fileNamePath01 });
+    	} else if(driver.toString().contains("firefox")) {
+    		WebElement uploadFile = driver.findElement(By.cssSelector(".fileinput-button"));
+        	uploadFile.click();
+        	Thread.sleep(1000);
+        	//execute file exe
+        	Runtime.getRuntime().exec(new String[] { chromePath, fileNamePath01 });
+    	}
+    	Thread.sleep(4000);
+    }
+    
+    @Test
+    public void TC_04_Robot() throws Exception {
+    	driver.get("http://blueimp.github.com/jQuery-File-Upload/");
+    	
+    	// Specify the file location with extension
+        StringSelection select = new  StringSelection(fileNamePath01);
+
+        // Copy to clipboard
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(select, null);
+        
+        if (driver.toString().contains("chrome")  || driver.toString().contains("firefox")) {
+            WebElement uploadFile =  driver.findElement(By.cssSelector(".fileinput-button"));
+            uploadFile.click();
+            Thread.sleep(1000);
+        } else {
+            System.out.println("Go to IE");
+            WebElement uploadFile =  driver.findElement(By.xpath("//input[@type='file']"));
+            clickToElementByJS(uploadFile);
+            Thread.sleep(1000);
+        }
+    	
+        Robot robot = new Robot();
+        Thread.sleep(1000);
+
+        // Nhan phim Enter
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+        // Nhan xuong Ctrl - V
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+
+        // Nha Ctrl - V
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyRelease(KeyEvent.VK_V);
+        Thread.sleep(1000);
+
+        // Nhan Enter
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+    
     @AfterTest
     public void afterTest() {
     	driver.quit();
     	
+    }
+    
+    public Object clickToElementByJS(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return js.executeScript("arguments[0].click();", element);
     }
     
  }
